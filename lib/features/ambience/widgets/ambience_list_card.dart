c34f63e4709438fb4c533a2f34412dae078ebdb4
nilenjay <nilenjaysinghsengar2006@gmail.com>
2026-04-22 01:06:32 +0530

    Edited home screen

--- lib/features/ambience/widgets/ambience_list_card.dart
+++ lib/features/ambience/widgets/ambience_list_card.dart
@@ -1,41 +1,78 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 import '../../../data/models/ambience_model.dart';
 
-class AmbienceListCard extends StatelessWidget{
+class AmbienceListCard extends StatelessWidget {
   final Ambience item;
-  
+
   const AmbienceListCard({super.key, required this.item});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16,0,16,16),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey.shade100,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.vertical(
-                top: Radius.circular(20),
+    return GestureDetector(
+      onTap: () {
+        context.pushNamed(
+          'details',
+          extra: item,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.shade100,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              // IMAGE
+              ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+                child: Image.asset(
+                  item.image,
+                  height: 140,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Image.asset(
-                item.image,
-                height: 140,
-                width: double.infinity,
-                fit: BoxFit.cover,
+
+              // TEXT
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item.tag.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      item.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      "${item.duration ~/ 60} min",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Text(item.title),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/features/ambience/widgets/featured_card.dart
+++ lib/features/ambience/widgets/featured_card.dart
@@ -1,66 +1,98 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 import '../../../data/models/ambience_model.dart';
 
-class FeaturedCard extends StatelessWidget{
+class FeaturedCard extends StatelessWidget {
   final Ambience item;
 
   const FeaturedCard({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return GestureDetector(
+      onTap: () {
+        context.pushNamed(
+          'details',
+          extra: item,
+        );
+      },
+      child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(24),
-        child: Stack(
-          children: [
-            Image.asset(
-              item.image,
-              height: 220,
-              width: double.infinity,
-              fit: BoxFit.cover,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(24),
+          child: Stack(
+            children: [
 
-            ),
-            Container(
-              height: 220,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
+              Image.asset(
+                item.image,
+                height: 220,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+
+              Container(
+                height: 220,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
                     colors: [
                       Colors.transparent,
                       Colors.black.withOpacity(0.7),
                     ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              bottom: 16,
-              left: 16,
-              right: 16,
-              child: Text(
-                item.title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+
+              Positioned(
+                bottom: 16,
+                left: 16,
+                right: 70,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item.tag.toUpperCase(),
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 10,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      item.title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "${item.duration ~/ 60} min",
+                      style: const TextStyle(
+                        color: Colors.white70,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Positioned(
-              bottom: 16,
-              right: 16,
-              child: CircleAvatar(
-                backgroundColor: Colors.blue,
-                child: const Icon(Icons.play_arrow, color: Colors.white,),
+
+              Positioned(
+                bottom: 16,
+                right: 16,
+                child: CircleAvatar(
+                  radius: 24,
+                  backgroundColor: Colors.blue,
+                  child: const Icon(
+                    Icons.play_arrow,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
